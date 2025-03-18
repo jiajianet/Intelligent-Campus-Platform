314e9427678c98eda0412b038ae4e2669e33d223
aliangliang51 <2931769856@qq.com>
2025-03-18 23:03:26 +0800

    更新菜单结构和切换到@aliangliang51的数据库的本地环境配置

--- Frontend-admin/react-app/src/pages/Layout/index.js
+++ Frontend-admin/react-app/src/pages/Layout/index.js
@@ -6,7 +6,10 @@ import {
     EditOutlined,
     LogoutOutlined,
     MenuFoldOutlined,
-    MenuUnfoldOutlined
+    MenuUnfoldOutlined,
+    PieChartOutlined,
+    AreaChartOutlined,
+    FormOutlined
 } from '@ant-design/icons'
 import './index.scss'
 import { Outlet, useLocation, useNavigate } from 'react-router-dom'
@@ -17,19 +20,128 @@ const { Header, Sider, Content } = Layout
 
 const items = [
     {
-        label: '首页',
-        key: '/',
-        icon: <HomeOutlined />,
+        label: '仪表盘',
+        key: '/dashboard',
+        icon: <PieChartOutlined/>,
+        children: [
+            {
+                label: '首页',
+                key: '/',
+                icon: <HomeOutlined/>,
+            },
+        ]
     },
     {
         label: '文章管理',
-        key: '/article',
-        icon: <DiffOutlined />,
+        key: '/',
+        icon: <HomeOutlined/>,
+        children: [
+            {
+                label: '文章管理',
+                key: '/article',
+                icon: <DiffOutlined/>,
+            },
+            {
+                label: '创建文章',
+                key: '/publish',
+                icon: <EditOutlined/>,
+            },
+            {
+                label: '主页轮廓图',
+                key: '/homePage',
+                icon: <FormOutlined/>
+            },
+        ]
     },
     {
-        label: '创建文章',
-        key: '/publish',
-        icon: <EditOutlined />,
+        label: '用户管理',
+        key: '/user',
+        icon: <HomeOutlined/>,
+        children: [
+            {
+                label: '用户列表',
+                key: '/userList',
+                icon: <DiffOutlined/>,
+            },
+            {
+                label: '创建用户',
+                key: '/createUser',
+                icon: <EditOutlined/>,
+            },
+        ]
+    },
+    {
+        label: '系统管理',
+        key: '/system',
+        icon: <HomeOutlined/>,
+        children: [
+            {
+                label: '系统设置',
+                key: '/systemSetting',
+                icon: <DiffOutlined/>,
+            },
+            {
+                label: '角色管理',
+                key: '/role',
+                icon: <EditOutlined/>,
+            },
+        ]
+    },
+    {
+        label: '菜单管理',
+        key: '/menu',
+        icon: <MenuFoldOutlined/>,
+        children: [
+            {
+                label: '菜单列表',
+                key: '/menuList',
+                icon: <MenuUnfoldOutlined/>,
+            },
+            {
+                label: '创建菜单',
+                key: '/createMenu',
+                icon: <MenuUnfoldOutlined/>,
+            },
+        ]
+    },
+    {
+        label: '运维管理',
+        key: '/operation',
+        icon: <MenuFoldOutlined/>,
+        children: [
+            {
+                label: '日志管理',
+                key: '/log',
+                icon: <MenuUnfoldOutlined/>,
+            },
+            {
+                label: '定时任务',
+                key: '/task',
+                icon: <MenuUnfoldOutlined/>,
+            },
+        ]
+    },
+    {
+        label: '帮助中心',
+        key: '/help',
+        icon: <MenuFoldOutlined/>,
+        children: [
+            {
+                label: '使用文档',
+                key: '/document',
+                icon: <MenuUnfoldOutlined/>,
+            },
+            {
+                label: '常见问题',
+                key: '/question',
+                icon: <MenuUnfoldOutlined/>,
+            },
+        ]
+    },
+    {
+        label: '更新日志',
+        key: '/update',
+        icon: <AreaChartOutlined />
     }
 ]
 
