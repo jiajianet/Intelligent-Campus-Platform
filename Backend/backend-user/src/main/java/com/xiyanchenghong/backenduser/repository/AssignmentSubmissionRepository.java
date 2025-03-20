6d762fc6cdd34fdaab364329a9c5f0855d85caae
Chenchen994 <1074711419@qq.com>
2025-03-20 10:18:34 +0800

    补交

--- Backend/backend-user/src/main/java/com/xiyanchenghong/backenduser/repository/AssignmentSubmissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.xiyanchenghong.backenduser.repository;
-
-import com.xiyanchenghong.backenduser.domain.AssignmentSubmission;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AssignmentSubmissionRepository extends JpaRepository<AssignmentSubmission, Long> {
-}
--- Backend/backend-user/src/main/java/com/xiyanchenghong/backenduser/repository/ExamSubmissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.xiyanchenghong.backenduser.repository;
-
-import com.xiyanchenghong.backenduser.domain.ExamSubmission;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExamSubmissionRepository extends JpaRepository<ExamSubmission, Long> {
-}
