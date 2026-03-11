7c850dc865c27499b475e76377c15de0b64f0193
zyimax <153522530+zyimax@users.noreply.github.com>
2026-03-11 16:32:19 +0800

    重命名ArticleServicelmpl类为ArticleServiceImpl，并修复相关引用

--- Backend/backend-user/src/main/java/com/xiyanchenghong/backenduser/controller/ArticleController.java
+++ Backend/backend-user/src/main/java/com/xiyanchenghong/backenduser/controller/ArticleController.java
@@ -1,7 +1,7 @@
 package com.xiyanchenghong.backenduser.controller;
 
 import com.xiyanchenghong.backenduser.domain.Article;
-import com.xiyanchenghong.backenduser.service.serviceImpl.ArticleServicelmpl;
+import com.xiyanchenghong.backenduser.service.serviceImpl.ArticleServiceImpl;
 import com.xiyanchenghong.backenduser.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,10 +15,10 @@ import java.util.List;
 @RequestMapping("/user")
 public class ArticleController {
 
-    private final ArticleServicelmpl articleService;
+    private final ArticleServiceImpl articleService;
 
     @Autowired
-    public ArticleController(ArticleServicelmpl articleService) {
+    public ArticleController(ArticleServiceImpl articleService) {
         this.articleService = articleService;
     }
 
--- Backend/backend-user/src/main/java/com/xiyanchenghong/backenduser/service/serviceImpl/ArticleServiceImpl.java
+++ Backend/backend-user/src/main/java/com/xiyanchenghong/backenduser/service/serviceImpl/ArticleServiceImpl.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
-public class ArticleServicelmpl implements ArticleService {
+public class ArticleServiceImpl implements ArticleService {
 
     @Value("${upload.dir}")
     private String uploadDir;
